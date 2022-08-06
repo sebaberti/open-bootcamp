bef13bccc13fdefe5df0402b3894516f473e1593
sebastian <sebaberti.8@gmail.com>
2022-08-05 22:38:18 -0300

    corregido

---
+++ listajs.js
@@ -0,0 +1,15 @@
+const lista = [
+    "Sebastian",
+    26,
+    false,
+    new Date(1996, 8, 2),
+    {
+        titulo: "Siddharta",
+        autor: "Herman Hesse",
+        fecha: new Date(1992,5 ,5),
+        url: "https://es.wikipedia.org/wiki/Siddhartha_(novela)"
+    },
+
+];
+
+console.log(lista)
