b31b63d26d97ec87337b60451aaa88d4711ae9bc
sebastian <sebaberti.8@gmail.com>
2022-08-07 12:25:05 -0300

    ej.numbers

---
+++ numbers.js
@@ -0,0 +1,9 @@
+
+const altura_cm = 165
+const altura_m = 1.65
+const peso_kg = 62.2
+
+const altura_red = Math.ceil(altura_m)
+const peso_red = Math.floor(peso_kg)
+
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
